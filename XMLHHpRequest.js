7975cdb6b373e9a9d4d447028427a5dbb9641c23
lushilling <lshilling@hotmail.co.uk>
2019-06-12 16:24:05 +0100

    day3

--- XMLHHpRequest.js
+++ XMLHHpRequest.js
@@ -1,59 +1,117 @@
+function makeReq(requestType, url, toSend) {
+   return new Promise((resolve, reject) => {
 
-let request = new XMLHttpRequest();
+       let request = new XMLHttpRequest();
+        request.onload = () => {
+           if(request.status == 200) {
+              resolve(request);
+             }
+           else { const reason = new Error ("Page not found/working");
+               reject(reason);
+           };
+         };
+       request.open(requestType, url);
+       request.send(toSend);
+    });
 
-// Get all accounts
-function getAllAccounts(){
-  request.onload = function(){
-    console.log('it worked!', request.responseText);
-    result.innerText = request.responseText;
-  };
-  request.open("GET", "http://localhost:8080/AccountSETemplate/api/account/getAllAccounts");
-  request.send();
 };
 
-//get an account
-function getAnAccount(idNum){
-  let id=idNum.value;
-
-  request.onload = function(){
-    console.log('it worked', request.responseText);
-    result.innerText = request.responseText;
-  };
-  request.open("GET", `http://localhost:8080/AccountSETemplate/api/account/getAnAccount/${id}`);
-  request.send();
+function onGetAllAccountsClicked() {
+   makeReq("GET", "http://localhost:8080/AccountSETemplate/api/account/getAllAccounts").then((res) => {
+     console.log('it worked!', res.responseText);
+     result.innerText = res.responseText;
+   }).catch(function (error){
+     console.log(error.message);
+   });
 };
 
-
-//createAccount
-function createAccount(){
-  const account = {
-    id:
-    accountNumber:
-    firstName:
-    lastName:
-  };
-
-  request.onload = function(){
-  console.log('it worked', request.responseText);
-  }
-
-  request.open("PUT", "http://localhost:8080/AccountSETemplate/api/account/createAccount");
-  request.send(JSON.stringify(acccount));
-}
-
-
-//deleteAccount
-function deleteAccount(idNum){
+function onGetAnAccountClicked(){
   let id=idNum.value;
+  makeReq("GET", `http://localhost:8080/AccountSETemplate/api/account/getAnAccount/${id}`).then((res) => {
+    console.log('it worked', res.responseText);
+    result.innerText = res.responseText;
+  }).catch(function (error){
+    console.log(error.message);
+  });
+};
 
-  request.onload = function(){
-  console.log('it worked', request.responseText);
-  result.innerText = request.responseText;
-  }
+function onGetCreateAccountClicked(){
+  const newAccount = {
+    accountNumber: accountNum.value,
+    firstName: fName.value,
+    lastName: lName.value
+  };
+  makeReq("POST", "http://localhost:8080/AccountSETemplate/api/account/createAccount", JSON.stringify(newAccount)).then((res) => {
+    console.log('it worked', res.responseText);
+    result.innerText = res.responseText;
+  }).catch(function (error){
+    console.log(error.message);
+  });
+};
 
-  request.open("DELETE", `http://localhost:8080/AccountSETemplate/api/account/deleteAccount/${id}`);
-  request.send();
-}
+function onDeleteAccountClicked(){
+  let id=idDelete.value;
+  makeReq("DELETE", `http://localhost:8080/AccountSETemplate/api/account/deleteAccount/${id}`).then((res) => {
+    console.log('it worked', res.responseText);
+    result.innerText = res.responseText;
+    }).catch(function (error){
+      console.log(error.message);
+    });
+};
+
+// //deleteAccount
+// function deleteAccount(){
+//   let id=idDelete.value;
+//
+//   request.onload = function(){
+//   console.log('it worked', request.responseText);
+//   result.innerText = request.responseText;
+//   };
+//
+//   request.open("DELETE", `http://localhost:8080/AccountSETemplate/api/account/deleteAccount/${id}`);
+//   request.send();
+// };
+
+// //createAccount
+// function createAccount(){
+//   const newAccount = {
+//     accountNumber: accountNum.value,
+//     firstName: fName.value,
+//     lastName: lName.value
+//   };
+//   console.log(newAccount);
+//   let request = new XMLHttpRequest();
+//   request.onload = function(){
+//     console.log('it worked', request.responseText);
+//     result.innerText = request.responseText;
+//   };
+//   request.open("POST", "http://localhost:8080/AccountSETemplate/api/account/createAccount");
+//   request.send(JSON.stringify(newAccount));
+// };
+
+//
+// // Get all accounts
+// function getAllAccounts(){
+//   let request = new XMLHttpRequest();
+//   request.onload = function(){
+//     console.log('it worked!', request.responseText);
+//     result.innerText = request.responseText;
+//   };
+//   request.open("GET", "http://localhost:8080/AccountSETemplate/api/account/getAllAccounts");
+//   request.send();
+// };
+
+// //get an account
+// function getAnAccount(){
+//   let id=idNum.value;
+//   let request = new XMLHttpRequest();
+//     request.onload = function(){
+//       console.log('it worked', request.responseText);
+//       result.innerText = request.responseText;
+//   };
+//   request.open("GET", `http://localhost:8080/AccountSETemplate/api/account/getAnAccount/${id}`);
+//   request.send();
+// };
 
 // //updateAccount
 // function updateAccount(idNum){
@@ -67,4 +125,4 @@ function deleteAccount(idNum){
 //   request.open("PUT", `http://localhost:8080/AccountSETemplate/api/account/updateAccount/${id}`);
 //   request.send();
 //
-// }
+//}
